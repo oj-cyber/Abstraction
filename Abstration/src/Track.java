700ece20e938c2d4e0c492435ad6c57c6d577b8b
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-11-03 22:13:29 +0300

    Commit 2
    In the new version on line 2,4,6 and 12 , I replaced load capacity with mileage.
    
    On line

--- Abstration/src/Track.java
+++ Abstration/src/Track.java
@@ -1,15 +1,16 @@
 public class Track extends vehicle{
-    private double loadcapacity;
+    private double mileage;
 
-    public Track(String make, String model, int year, double loadcapacity) {
-        super(make, model, year);
-        this.loadcapacity = loadcapacity;
+    public Track(String make, String model, int year, double mileage,double price) {
+        super(make, model, year,price);
+        this.mileage = mileage;
     }
     public void displaydetails(){
         System.out.println("Make:"+" "+make);
         System.out.println("Model:"+" "+model);
         System.out.println("Year:"+" "+year);
-        System.out.println("Load capacity:"+" "+loadcapacity+"tons");
+        System.out.println("Mileage:"+" "+mileage+"km/h");
+        System.out.println("Price:"+" "+" "+"$"+price);
 
     }
     public void StartEngine() {
