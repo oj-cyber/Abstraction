dfad58564ed3a90a0598c0dbd1443ba0dfa527c7
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-11-03 22:19:19 +0300

    Commit 2
    In the new version on line 4,9 and 15, I added the price arguments

--- Abstration/src/Main.java
+++ Abstration/src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("====Car===");
-        car car=new car("Toyota","Camry",2020,"blue");
+        car car=new car("Toyota","Camry",2020,"blue",7000);
         car.displaydetails(); car.StartEngine();
         System.out.println();
 
         System.out.println("====Motor Cycle===");
-        Bike bike=new Bike("Kawasaki","BMW",2023,"Touring","Two wheeler");
+        Bike bike=new Bike("Kawasaki","BMW",2023,"Touring","Two wheeler",10000);
         bike.displaydetails(); bike.StartEngine();
         System.out.println();
 
 
         System.out.println("====Track===");
-        Track track=new Track("Ford","F-150",2020,2.5);
+        Track track=new Track("Ford","F-150",2020,2.5,9900);
         track.displaydetails(); track.StartEngine();
 
 
