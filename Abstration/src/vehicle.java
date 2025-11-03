8a38863a1c449a1fe2427d43e7189a58bbe66965
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-11-03 21:02:44 +0300

    Commit 2
    In the new version on line 5 and 7 ,I added a new instace variable price.
    On line 6, I deleted the constructor.
    on line 11,I created a new empty line

--- Abstration/src/vehicle.java
+++ Abstration/src/vehicle.java
@@ -2,11 +2,13 @@ public abstract class vehicle {
     protected String make;
     protected String model;
     protected int year;
+    protected  double price;
 
-    public vehicle(String make, String model, int year) {
+    public vehicle(String make, String model, int year,double price) {
         this.make = make;
         this.model = model;
         this.year = year;
+
     }
     public abstract void displaydetails();
     public void StartEngine(){
